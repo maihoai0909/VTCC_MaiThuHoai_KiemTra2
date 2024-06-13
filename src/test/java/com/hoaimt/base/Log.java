4ed90c7a55290d047bf5638dac5c44339bc16e50
maihoai0909 <parkshinehye99@gmail.com>
2024-06-13 20:13:45 +0700

    commit code

---
+++ src/test/java/com/hoaimt/base/Log.java
@@ -0,0 +1,45 @@
+package com.hoaimt.base;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log {
+    //Initialize Log4j instance
+    private static final Logger Log =  LogManager.getLogger(Log.class);
+
+    //Info Level Logs
+    public static void info (String message) {
+        com.hoaimt.base.Log.info(message);
+    }
+    public static void info (Object object) {
+        com.hoaimt.base.Log.info(object);
+    }
+
+    //Warn Level Logs
+    public static void warn (String message) {
+        com.hoaimt.base.Log.warn(message);
+    }
+    public static void warn (Object object) {
+        com.hoaimt.base.Log.warn(object);
+    }
+
+    //Error Level Logs
+    public static void error (String message) {
+        com.hoaimt.base.Log.error(message);
+    }
+    public static void error (Object object) {
+        com.hoaimt.base.Log.error(object);
+    }
+
+    //Fatal Level Logs
+    public static void fatal (String message) {
+        Log.fatal(message);
+    }
+
+    //Debug Level Logs
+    public static void debug (String message) {
+        Log.debug(message);
+    }
+    public static void debug (Object object) {
+        Log.debug(object);
+    }
+}
